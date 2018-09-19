853e6b91211e1ed2e9f66653d292ba6fd7f83ae5
faisal ridwan <faisal.ridwan55@gmail.com>
2018-09-19 16:06:35 +0700

    update pdf dan comment method challenge karena di handle method challengePath

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,15 +16,19 @@ public class PageController {
 		return "viral";
 	}
 	
-	@RequestMapping("/challenge")
-	public String challenge(@RequestParam(value = "name") String name, Model model) {
-		model.addAttribute("name", name);
-		return "challenge";
-	}
+//	@RequestMapping("/challenge")
+//	public String challenge(@RequestParam(value = "name") String name, Model model) {
+//		model.addAttribute("name", name);
+//		return "challenge";
+//	}
 	
-	@RequestMapping("/challenge/{name}")
-	public String challengePath(@PathVariable String name, Model model) {
-		model.addAttribute("name", name);
+	@RequestMapping(value = {"/challenge","/challenge/{name}"})
+	public String challengePath(@PathVariable Optional<String> name, Model model) {
+		if (name.isPresent()) {
+			model.addAttribute("name", name.get());			
+		} else {
+			model.addAttribute("name", "KB");
+		}
 		return "challenge";
 	}
 	
